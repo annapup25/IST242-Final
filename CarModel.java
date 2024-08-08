f10fa527ac2b33746fc21ba843a72799658361d9
JoshRunja <runja.joshua@gmail.com>
2024-08-07 23:53:08 -0400

    with comments

--- CarModel.java
+++ CarModel.java
@@ -1,9 +1,10 @@
 public class CarModel {
-    private int modelId;
-    private String modelName;
-    private int modelBasePrice;
-    private int brandId;
+    private int modelId; // ID of the car model
+    private String modelName; // name of the car model
+    private int modelBasePrice; // base price of the car model
+    private int brandId; // ID of the brand
 
+    // initialize a CarModel object
     public CarModel(int modelId, String modelName, int modelBasePrice, int brandId) {
         this.modelId = modelId;
         this.modelName = modelName;
@@ -16,34 +17,42 @@ public class CarModel {
         return modelId;
     }
 
+    // Setter for model ID
     public void setModelId(int modelId) {
         this.modelId = modelId;
     }
 
+    // Getter for model name
     public String getModelName() {
         return modelName;
     }
 
+    // Setter for model name
     public void setModelName(String modelName) {
         this.modelName = modelName;
     }
 
+    // Getter for model base price
     public int getModelBasePrice() {
         return modelBasePrice;
     }
 
+    // Setter for model base price
     public void setModelBasePrice(int modelBasePrice) {
         this.modelBasePrice = modelBasePrice;
     }
 
+    // Getter for brand ID
     public int getBrandId() {
         return brandId;
     }
 
+    // Setter for brand ID
     public void setBrandId(int brandId) {
         this.brandId = brandId;
     }
 
+    //  toString method of the CarModel object
     @Override
     public String toString() {
         return "CarModel{" +
--- Dealer.java
+++ Dealer.java
@@ -1,29 +1,34 @@
 public class Dealer {
-    private int dealerId;
-    private String dealerName;
+    private int dealerId; // ID of the dealer
+    private String dealerName; // Name of the dealer
 
+    // constructor to initialize a Dealer object
     public Dealer(int dealerId, String dealerName) {
         this.dealerId = dealerId;
         this.dealerName = dealerName;
     }
 
-    // Getters and Setters
+    // Getter for dealer ID
     public int getDealerId() {
         return dealerId;
     }
 
+    // Setter for dealer ID
     public void setDealerId(int dealerId) {
         this.dealerId = dealerId;
     }
 
+    // Getter for dealer name
     public String getDealerName() {
         return dealerName;
     }
 
+    // Setter for dealer name
     public void setDealerName(String dealerName) {
         this.dealerName = dealerName;
     }
 
+    // toString method of the Dealer object
     @Override
     public String toString() {
         return "Dealer{" +
